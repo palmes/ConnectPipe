2ed676c329a8a70092ae8e0101086475a73b4e93
palmes <68684325+palmes@users.noreply.github.com>
2020-07-23 22:41:38 +0200

    Initial commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Prefab for adding the tiles
+    public GameObject prefab1;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void AddTile(float x_pos, float y_pos)
+    {
+        Instantiate(prefab1, new Vector3(x_pos, y_pos), Quaternion.identity);
+    }
+}
---
+++ Assets/Scripts/PlacementArrow.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlacementArrow : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // add a new instance of that prefab thingy at current position
+    private void OnMouseDown()
+    {
+        Debug.Log("At position " + x_pos + y_pos);
+        GameObject.Find("GameManager").GetComponent<GameManager>().AddTile(x_pos, y_pos);
+    }
+}
