b6877e4b86b7e1f3fa3ba34016e61c00915c6248
palmes <68684325+palmes@users.noreply.github.com>
2020-07-24 19:58:12 +0200

    Generate tile sequence and push to stack

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Data;
 using System.Reflection;
 using UnityEngine;
 
@@ -7,7 +8,13 @@ public class GameManager : MonoBehaviour
 {
     // Prefab for adding the tiles
     public GameObject tilePrefab0, tilePrefab3, tilePrefab5, tilePrefab6, tilePrefab9, tilePrefab10, tilePrefab12;
+    public int gridSize = 4;
+    public int leftExit, rightExit;
     private Dictionary<int, GameObject> tilePrefabs = new Dictionary<int, GameObject>();
+    private int[,] originalGrid;
+    private int[,] currentGrid;
+    private Stack<int> tileSequence;
+
 
     // Start is called before the first frame update
     void Start()
@@ -20,7 +27,8 @@ public class GameManager : MonoBehaviour
         tilePrefabs.Add(9, tilePrefab9);
         tilePrefabs.Add(10, tilePrefab10);
         tilePrefabs.Add(12, tilePrefab12);
-        SetupTileSequence(4);
+        SetupTileSequence();
+        DisplayOriginalGrid();
     }
 
     // Update is called once per frame
@@ -37,75 +45,93 @@ public class GameManager : MonoBehaviour
         Instantiate(selectedPrefab, new Vector3(x_pos, y_pos), Quaternion.identity);
     }
 
-    private void SetupTileSequence(int gridsize)
+    private void SetupTileSequence()
     {
-        int[,] grid = new int[gridsize, gridsize];
-        int[] transitions = new int[gridsize + 1];
+        originalGrid = new int[gridSize, gridSize];
+        int[] transitions = new int[gridSize + 1];
         // I want to create a solvable puzzle, so the original solution (there will be many more) will be created in an easy way.
         // Going only from left to right and allowing the pipe to go up and down in between. Such a pipe can be defined by just 
         // the y values at the transitions from one column to the next.
-        for (int i = 0; i < gridsize + 1; i++)
+        for (int i = 0; i < gridSize + 1; i++)
         {
-            transitions[i] = Random.Range(0, gridsize);
+            transitions[i] = Random.Range(0, gridSize);
         }
+        leftExit = transitions[0];
+        rightExit = transitions[gridSize];
         // Now based on the randomly generated transitions, we determine which tile must be placed at every point of the grid.
-        for (int i = 0; i < gridsize; i++)
+        for (int i = 0; i < gridSize; i++)
         {
-            for (int j = 0; j < gridsize; j++)
+            for (int j = 0; j < gridSize; j++)
             {
                 if (transitions[j] > i)
                 {
                     if (transitions[j + 1] > i)
                     {
-                        grid[i, j] = 0;
+                        originalGrid[i, j] = Random.Range(3, 13);
                     }
                     else if (transitions[j + 1] == i)
                     {
-                        grid[i, j] = 6;
+                        originalGrid[i, j] = 6;
                     } else
                     {
-                        grid[i, j] = 5;
+                        originalGrid[i, j] = 5;
                     }
                 }
                 else if (transitions[j] == i)
                 {
                     if (transitions[j + 1] > i)
                     {
-                        grid[i, j] = 12;
+                        originalGrid[i, j] = 12;
                     }
                     else if (transitions[j + 1] == i)
                     {
-                        grid[i, j] = 10;
+                        originalGrid[i, j] = 10;
                     }
                     else
                     {
-                        grid[i, j] = 9;
+                        originalGrid[i, j] = 9;
                     }
                 }
                 else
                 {
                     if (transitions[j + 1] > i)
                     {
-                        grid[i, j] = 5;
+                        originalGrid[i, j] = 5;
                     }
                     else if (transitions[j + 1] == i)
                     {
-                        grid[i, j] = 3;
+                        originalGrid[i, j] = 3;
                     }
                     else
                     {
-                        grid[i, j] = 0;
+                        originalGrid[i, j] = Random.Range(3, 13);
                     }
                 }
             }
         } //end for loop filling the grid with integers representing the tiles
+        // Put them in a stack randomly, just taking from the top so it will be possible to reconstruct the pipe maze.
+        tileSequence = new Stack<int>();
+        int[] tops = new int[gridSize]; 
+        while (tileSequence.Count < gridSize*gridSize)
+        {
+            int column = Random.Range(0, gridSize);
+            if (tops[column]<gridSize)
+            {
+                tileSequence.Push(originalGrid[tops[column], column]);
+                tops[column]++;
+                Debug.Log("" + tileSequence.Count + ". " + tileSequence.Peek());
+            }
+        }
+    }
+
+    private void DisplayOriginalGrid() { 
         // Now to see how it all worked out, place the generated grid in the game area. Remove this later.
         // Create the outer walls (also out of tiles because possibly easier)
         GameObject selectedPrefab = tilePrefab0;
-        for (int i = 0; i < gridsize; i ++)
+        for (int i = 0; i < gridSize; i ++)
         {
             // Left Wall
-            if  (transitions[0] == i)
+            if  (leftExit == i)
             {
                 selectedPrefab = tilePrefab10;
             }
@@ -113,9 +139,9 @@ public class GameManager : MonoBehaviour
             {
                 selectedPrefab = tilePrefab0;
             }
-            Instantiate(selectedPrefab, new Vector3(0.65f * (-1.0f - 0.5f * (gridsize - 1)), gridsize - i - 1.0f), Quaternion.identity);
+            Instantiate(selectedPrefab, new Vector3(0.65f * (-1.0f - 0.5f * (gridSize - 1)), gridSize - i - 1.0f), Quaternion.identity);
             // Right wall
-            if (transitions[gridsize] == i)
+            if (rightExit == i)
             {
                 selectedPrefab = tilePrefab10;
             }
@@ -123,15 +149,15 @@ public class GameManager : MonoBehaviour
             {
                 selectedPrefab = tilePrefab0;
             }
-            Instantiate(selectedPrefab, new Vector3(0.65f * (1.0f + 0.5f * (gridsize - 1)), gridsize - i - 1.0f), Quaternion.identity);
+            Instantiate(selectedPrefab, new Vector3(0.65f * (1.0f + 0.5f * (gridSize - 1)), gridSize - i - 1.0f), Quaternion.identity);
             // And now the real deal, the contents of the grid
-            for (int j = 0; j < gridsize; j++)
+            for (int j = 0; j < gridSize; j++)
             {
                 // Print for debugging purposes
-                Debug.Log(grid[i, j]);
+                // Debug.Log(originalGrid[i, j]);
                 // And now place the tiles
-                if (!tilePrefabs.TryGetValue(grid[i, j], out selectedPrefab)) selectedPrefab = tilePrefab0;
-                Instantiate(selectedPrefab, new Vector3(0.65f*(-0.5f * (gridsize - 1) + j), gridsize - i - 1.0f), Quaternion.identity);
+                if (!tilePrefabs.TryGetValue(originalGrid[i, j], out selectedPrefab)) selectedPrefab = tilePrefab0;
+                Instantiate(selectedPrefab, new Vector3(0.65f*(-0.5f * (gridSize - 1) + j), gridSize - i - 1.0f), Quaternion.identity);
             }
         }
         
